b45e366053c51c7f7f9373fe74ba5dcf8655fe00
linguocheng <linguocheng@dashuf.com>
2018-12-05 11:27:54 +0800

    fix:修改一级渠道删除出错问题

--- backend/src/main/java/com/feitai/admin/backend/config/web/ChannelPrimaryController.java
+++ backend/src/main/java/com/feitai/admin/backend/config/web/ChannelPrimaryController.java
@@ -13,9 +13,14 @@ import com.feitai.admin.backend.config.service.ChannelPrimaryService;
 import com.feitai.admin.backend.config.service.ChannelService;
 import com.feitai.admin.backend.properties.MapProperties;
 import com.feitai.admin.core.service.DynamitSupportService;
+import com.feitai.admin.core.service.OnCondition;
+import com.feitai.admin.core.service.Operator;
+import com.feitai.admin.core.service.SearchParams;
+import com.feitai.admin.core.service.SelectMultiTable;
 import com.feitai.admin.core.vo.AjaxResult;
 import com.feitai.admin.core.vo.ListItem;
 import com.feitai.admin.core.web.BaseListableController;
+import com.feitai.jieya.server.dao.channel.model.Channel;
 import com.feitai.utils.SnowFlakeIdGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -128,14 +133,23 @@ public class ChannelPrimaryController extends BaseListableController<ChannelPrim
 	public Object del(@RequestParam(value = "ids[]") Long[] ids){
 		boolean delAllow = true;
 		for(Long id:ids){
-			if(channelService.findByPrimaryId(id).size()!=0) {
+			if(channelService.countBySqls(getCountSql(id),SelectMultiTable.COUNT_ALIAS)!=0){
 				return new AjaxResult(false, "id为：[" + id + "] 的一级渠道尚有对应二级渠道未删除");
 			}
 		}
 		this.channelPrimaryService.delete(ids);
 		return this.successResult;
 	}
-	
+	private  String getCountSql(Long id) {
+		StringBuffer countSql=new StringBuffer();
+		String sql=SelectMultiTable.builder(Channel.class).leftJoin(ChannelPrimary.class, "channelPrimary", new OnCondition[]{
+                        new OnCondition(SelectMultiTable.ConnectType.AND, "mainPackageCode", Operator.EQ, "channelCode")
+                }).buildCountSqlString();
+		countSql.append(sql)
+		        .append(" where channelPrimary.id=")
+		        .append(id);
+		return countSql.toString();
+	}
 
 	/**
 	 * 所有RequestMapping方法调用前的Model准备方法, 实现Struts2 Preparable二次部分绑定的效果,先根据form的id从数据库查出User对象,再把Form提交的内容绑定到该对象上。
