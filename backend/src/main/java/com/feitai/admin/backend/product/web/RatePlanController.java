b7e808959c849a0aa43c37a9318d52628ca63c71
linguocheng <linguocheng@dashuf.com>
2019-01-10 18:40:36 +0800

    fix():将费率方案增删改放到service层

--- backend/src/main/java/com/feitai/admin/backend/product/web/RatePlanController.java
+++ backend/src/main/java/com/feitai/admin/backend/product/web/RatePlanController.java
@@ -122,62 +122,7 @@ public class RatePlanController extends BaseListableController<RatePlanMore> {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     @ResponseBody
     public Object add(@Valid @RequestBody RatePlanRequest ratePlanRequest) {
-        Date date = new Date();
-        ratePlanRequest.setId(null);
-        RatePlanMore ratePlan = new RatePlanMore();
-        BeanUtils.copyProperties(ratePlanRequest, ratePlan);        
-        ratePlan.setCurrentVersion(0);
-        ratePlan = ratePlanService.save(ratePlan);
-        //复制到快照表
-        SnapshotRatePlan snapshotRatePlan = new SnapshotRatePlan();
-        BeanUtils.copyProperties(ratePlan, snapshotRatePlan);
-        snapshotRatePlan.setId(null);
-        snapshotRatePlan = snapshotRatePlanService.save(snapshotRatePlan);
-        for (Weight weight : ratePlanRequest.getWeight()) {
-            RatePlanTermMore ratePlanTerm = new RatePlanTermMore();
-            ratePlanTerm.setRatePlanId(ratePlan.getId());
-            ratePlanTerm.setTerm(weight.getTerm());
-            ratePlanTerm.setWeight(weight.getWeight());
-            ratePlanTerm.setVersion("0");
-            ratePlanTerm = ratePlanTermService.save(ratePlanTerm);
-            // 复制到快照表
-            SnapshotRatePlanTerm snapshotRatePlanTerm = new SnapshotRatePlanTerm();
-            BeanUtils.copyProperties(ratePlanTerm, snapshotRatePlanTerm);
-            snapshotRatePlanTerm.setId(null);
-            snapshotRatePlanTerm.setRatePlanId(snapshotRatePlan.getId());
-            snapshotRatePlanTerm.setVersion(0);
-            snapshotRatePlanTerm = snapshotRatePlanTermService.save(snapshotRatePlanTerm);
-            for (FeePlan feePlan : ratePlanRequest.getFeePlan()) {
-                if (feePlan.getTerm().shortValue() == weight.getTerm()) {
-                    List<FeePlanDetail> feePlanDetailList = feePlan.getSubject();
-                    for (FeePlanDetail feePlanDetail : feePlanDetailList) {
-                        RatePlanDetail ratePlanDetail = new RatePlanDetail();
-                        BeanUtils.copyProperties(feePlanDetail, ratePlanDetail);
-                        ratePlanDetail.setRatePlanTermId(ratePlanTerm.getId());
-                        ratePlanDetail.setName(subjectMap.get(ratePlanDetail.getSubjectId()));
-                        ratePlanDetail.setVersion(0);
-                        // 利率除以100
-                        if (!CalculationMode.FIXED_AMOUNT.equals(ratePlanDetail.getCalculationMode())) {
-                            ratePlanDetail.setFee(feePlanDetail.getFee().divide(new BigDecimal("100")));
-                        }
-                        // 默认回写
-                        if (ratePlanDetail.getFeeBaseType() == null) {
-                            ratePlanDetail.setFeeBaseType(FeeBaseType.DEFAULT);
-                        }
-                        if (ratePlanDetail.getPaymentType() == null) {
-                            ratePlanDetail.setPaymentType(PaymentType.DEFAULT);
-                        }
-                        ratePlanDetailService.save(ratePlanDetail);
-                        // 复制到快照表
-                        RatePlanDetailSnapshot snapshotRatePlanDetail = new RatePlanDetailSnapshot();
-                        BeanUtils.copyProperties(ratePlanDetail, snapshotRatePlanDetail);
-                        snapshotRatePlanDetail.setId(null);
-                        snapshotRatePlanDetail.setRatePlanTermId(snapshotRatePlanTerm.getRatePlanId());
-                        snapshotRatePlanDetailService.save(snapshotRatePlanDetail);
-                    }
-                }
-            }
-        }
+    	ratePlanService.addRatePlan(ratePlanRequest);
         return successResult;
     }
 
@@ -185,76 +130,7 @@ public class RatePlanController extends BaseListableController<RatePlanMore> {
     @RequestMapping(value = "update", method = RequestMethod.POST)
     @ResponseBody
     public Object update(@RequestBody RatePlanRequest ratePlanRequest) {
-        RatePlanMore ratePlan = ratePlanService.findOne(ratePlanRequest.getId());
-        if (ratePlan != null) {
-            //保留code和版本信息，删除原来记录
-            Integer currentVersion = ratePlan.getCurrentVersion() + 1;
-            for (RatePlanTermMore ratePlanTerm : ratePlan.getRatePlanTerms()) {
-            	if(ratePlanTerm.getRatePlanDetails()!=null){
-	                for (RatePlanDetail ratePlanDetail : ratePlanTerm.getRatePlanDetails()) {
-	                    ratePlanDetailService.delete(ratePlanDetail);
-	                }
-            	}
-                ratePlanTermService.delete(ratePlanTerm);
-            }
-            ratePlanService.delete(ratePlan);
-            // 重新插入
-            Date date = new Date();
-            BeanUtils.copyProperties(ratePlanRequest, ratePlan);
-           // ratePlan.setId(null);
-            ratePlan.setCurrentVersion(currentVersion);
-            ratePlan.setCreatedTime(date);
-            ratePlan.setUpdateTime(date);
-            ratePlan = ratePlanService.save(ratePlan);
-            SnapshotRatePlan snapshotRatePlan = new SnapshotRatePlan();
-            BeanUtils.copyProperties(ratePlan, snapshotRatePlan);
-            snapshotRatePlan = snapshotRatePlanService.save(snapshotRatePlan);
-            for (Weight weight : ratePlanRequest.getWeight()) {
-                RatePlanTermMore ratePlanTerm = new RatePlanTermMore();
-                ratePlanTerm.setRatePlanId(ratePlan.getId());
-                ratePlanTerm.setTerm(weight.getTerm());
-                ratePlanTerm.setWeight(weight.getWeight());
-                ratePlanTerm.setVersion(currentVersion+"");
-                ratePlanTerm.setCreatedTime(date);
-                ratePlanTerm.setUpdateTime(date);
-                ratePlanTerm = ratePlanTermService.save(ratePlanTerm);
-                SnapshotRatePlanTerm snapshotRatePlanTerm = new SnapshotRatePlanTerm();
-                BeanUtils.copyProperties(ratePlanTerm, snapshotRatePlanTerm);
-                snapshotRatePlanTerm.setVersion(currentVersion);
-                snapshotRatePlanTerm.setRatePlanId(snapshotRatePlan.getId());
-                snapshotRatePlanTermService.save(snapshotRatePlanTerm);
-                for (FeePlan feePlan : ratePlanRequest.getFeePlan()) {
-                    if (feePlan.getTerm().shortValue() == weight.getTerm()) {
-                        List<FeePlanDetail> feePlanDetailList = feePlan.getSubject();
-                        for (FeePlanDetail feePlanDetail : feePlanDetailList) {
-                            RatePlanDetail ratePlanDetail = new RatePlanDetail();
-                            BeanUtils.copyProperties(feePlanDetail, ratePlanDetail);
-                            ratePlanDetail.setRatePlanTermId(ratePlanTerm.getId());
-                            ratePlanDetail.setName(subjectMap.get(ratePlanDetail.getSubjectId()));
-                           ratePlanDetail.setVersion(currentVersion);
-                            // 利率除以100
-                            if (!CalculationMode.FIXED_AMOUNT.equals(ratePlanDetail.getCalculationMode())) {
-                                ratePlanDetail.setFee(ratePlanDetail.getFee().divide(new BigDecimal("100")));
-                            }
-                            // 默认回写
-                            if (ratePlanDetail.getFeeBaseType() == null) {
-                                ratePlanDetail.setFeeBaseType(FeeBaseType.DEFAULT);
-                            }
-                            if (ratePlanDetail.getPaymentType() == null) {
-                                ratePlanDetail.setPaymentType(PaymentType.DEFAULT);
-                            }
-                            ratePlanDetail.setCreatedTime(date);
-                            ratePlanDetail.setUpdateTime(date);
-                            ratePlanDetailService.save(ratePlanDetail);
-                            RatePlanDetailSnapshot snapshotRatePlanDetail = new RatePlanDetailSnapshot();
-                            BeanUtils.copyProperties(ratePlanDetail, snapshotRatePlanDetail);
-                            snapshotRatePlanDetail.setRatePlanTermId(snapshotRatePlanTerm.getId());
-                            snapshotRatePlanDetailService.save(snapshotRatePlanDetail);
-                        }
-                    }
-                }
-            }
-        }
+    	ratePlanService.updateRatePlan(ratePlanRequest);
         return successResult;
     }
 
@@ -262,24 +138,7 @@ public class RatePlanController extends BaseListableController<RatePlanMore> {
     @RequestMapping(value = "del")
     @ResponseBody
     public Object del(@RequestParam(value = "ids[]") Long[] ids) {
-        for (Long id : ids) {
-            RatePlanMore ratePlan = ratePlanService.findOne(id);
-            if (ratePlan != null ) {
-                if(!CollectionUtils.isEmpty(ratePlan.getRatePlanTerms())){
-                    // 删除期数
-                    for (RatePlanTermMore ratePlanTerm : ratePlan.getRatePlanTerms()) {
-                        if (!CollectionUtils.isEmpty(ratePlanTerm.getRatePlanDetails())) {
-                            for (RatePlanDetail ratePlanDetail : ratePlanTerm.getRatePlanDetails()) {
-                                // 删除明细
-                                ratePlanDetailService.deleteByPrimaryKey(ratePlanDetail.getId());
-                            }
-                        }
-                        ratePlanTermService.deleteByPrimaryKey(ratePlanTerm.getId());
-                    }
-                }
-                ratePlanService.delete(ratePlan);
-            }
-        }
+        ratePlanService.delete(ids);
         return successResult;
     }
 
