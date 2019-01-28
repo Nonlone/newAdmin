828f8f1856aed1dc610a8eff6a8666b05dbcaa66
qiuyunlong <qiuyunlong@dashuf.com>
2019-01-28 17:52:14 +0800

    fix(): 提现订单，图片展示BUG

--- mop/src/main/java/com/feitai/admin/mop/superpartner/vo/OrderDetailInfo.java
+++ mop/src/main/java/com/feitai/admin/mop/superpartner/vo/OrderDetailInfo.java
@@ -1,7 +1,6 @@
 package com.feitai.admin.mop.superpartner.vo;
 
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.annotation.JSONField;
 import com.feitai.base.annotion.NoKeyFilter;
 import lombok.Data;
 
@@ -20,14 +19,5 @@ public class OrderDetailInfo {
     private String idCardFront;
     private String idCardReverse;
 
-    @JSONField(serialize = false)
     private JSONObject idCardDetail;
-
-    public String getPicFront() {
-        return (String)idCardDetail.get("front");
-    }
-
-    public String getPicReverse() {
-        return (String)idCardDetail.get("reverse");
-    }
 }
